e5029ad5550b516769f662d80d9e1ef9c81fb575
thejokers <thejokkers69@gmail.com>
2024-10-18 18:11:14 +0100

    Initial commit

---
+++ src/main/java/org/example/student_app_mohamed_lakssir/StudentAppMohamedLakssirApplication.java
@@ -0,0 +1,13 @@
+package org.example.student_app_mohamed_lakssir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAppMohamedLakssirApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentAppMohamedLakssirApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/student_app_mohamed_lakssir/StudentAppMohamedLakssirApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.student_app_mohamed_lakssir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAppMohamedLakssirApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
