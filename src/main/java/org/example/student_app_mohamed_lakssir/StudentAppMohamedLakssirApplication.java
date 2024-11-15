c63b8435fc1b78b6137bb7f0ae6c9632f23650b7
thejokers69 <thejokkers69@gmail.com>
2024-11-15 21:33:56 +0100

    Modified

--- src/main/java/org/example/student_app_mohamed_lakssir/StudentAppMohamedLakssirApplication.java
+++ src/main/java/org/example/student_app_mohamed_lakssir/StudentAppMohamedLakssirApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentAppMohamedLakssirApplication {
+public class    StudentAppMohamedLakssirApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(StudentAppMohamedLakssirApplication.class, args);
